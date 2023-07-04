7cac1b36b2df2ee45f052d7013c223b0f9f163d5
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-07-04 12:51:33 +0200

    Update updateFingerprints.py

--- updateFingerprints.py
+++ updateFingerprints.py
@@ -5,8 +5,6 @@ config.load_incluster_config()
 api_instance = client.CoreV1Api()
 cmap = client.V1ConfigMap()
 cmap.data = {}
-cmap.data["allowSnippetAnnotations"] = "false"
-cmap.data["enableSnippetDirectives"] = "false"
 cmap.data["http-snippet"] = "test"
 
 api_instance.patch_namespaced_config_map(namespace="kube-system",name="addon-http-application-routing-nginx-configuration", body=cmap)
