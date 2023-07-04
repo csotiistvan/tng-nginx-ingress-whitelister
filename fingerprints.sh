816108aeb9c7a361044ba015ebbc9cd362e7d859
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-07-04 12:45:10 +0200

    Update fingerprints.sh

--- fingerprints.sh
+++ fingerprints.sh
@@ -7,7 +7,7 @@ echo "default 0;" >> $nginx_conf
 
 for cert in $(/usr/bin/find $1 -path **/TLS/* -name TLS*.pem)
 do
-    fingerprint=$(openssl x509 -in "$cert" -noout -fingerprint -sha1 | sed 's/SHA1 Fingerprint=//; s/://g')
+    fingerprint=$(openssl x509 -in "$cert" -noout -fingerprint -sha1 | cut -d'=' -f 2)
     echo "$fingerprint 1;" >> $nginx_conf
 done
 
