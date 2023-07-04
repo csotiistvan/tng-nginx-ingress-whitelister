90d718be0abf5e19317bfe16df5fad2bc0565252
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-07-04 12:56:52 +0200

    Update updateFingerprints.py

--- updateFingerprints.py
+++ updateFingerprints.py
@@ -1,10 +1,14 @@
 from kubernetes import client, config
+import sys
 
 config.load_incluster_config()
+
+with open(sys.argv[1]) as f:
+    data = f.read()
 
 api_instance = client.CoreV1Api()
 cmap = client.V1ConfigMap()
 cmap.data = {}
-cmap.data["http-snippet"] = "test"
+cmap.data["http-snippet"] = data
 
 api_instance.patch_namespaced_config_map(namespace="kube-system",name="addon-http-application-routing-nginx-configuration", body=cmap)
