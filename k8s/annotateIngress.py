b9ecb5c91d5f7ce30881634f7e7b230e3a79dced
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-07-07 17:58:57 +0200

    annotate script

---
+++ k8s/annotateIngress.py
@@ -0,0 +1,10 @@
+from kubernetes import client, config
+import sys
+import os
+
+ingress_namespace = os.environ.get("INGRESS_NAMESPACE")
+ingress_selector=os.environ.get("INGRESS_SELECTOR")
+
+api_instance = client.CoreV1Api()
+ingress = api_instance.read_namespaced_ingress(namespace=ingress_namespace)
+print(ingress)
