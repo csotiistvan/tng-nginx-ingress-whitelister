181c3675cada9bcfe60aad5bc94b54f6191ddd1b
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-07-07 18:24:21 +0200

    Update annotateIngress.py

--- annotateIngress.py
+++ annotateIngress.py
@@ -3,8 +3,8 @@ import sys
 import os
 
 ingress_namespace = os.environ.get("INGRESS_NAMESPACE")
-ingress_selector=os.environ.get("INGRESS_SELECTOR")
+ingress_name=os.environ.get("INGRESS_SNAME")
 
 api_instance = client.NetworkingV1Api()
-ingress = api_instance.read_namespaced_ingress(namespace=ingress_namespace)
+ingress = api_instance.read_namespaced_ingress(namespace=ingress_namespace,name=ingress_name)
 print(ingress)
