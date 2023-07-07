729e29c7a1040116715e5cecd938e6e01b20a409
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-07-07 22:07:36 +0200

    Annotations added

--- annotateIngress.py
+++ annotateIngress.py
@@ -6,14 +6,16 @@ config.load_incluster_config()
 
 ingress_namespace = os.environ.get("INGRESS_NAMESPACE")
 ingress_name=os.environ.get("INGRESS_NAME")
+bundle_namespace = os.environ.get("BUNDLE_NAMESPACE")
+bundle_name =  os.environ.get("BUNDLE_NAME")
 
 api_instance = client.NetworkingV1Api()
 ingress = api_instance.read_namespaced_ingress(name=ingress_name,namespace=ingress_namespace)
 
 if ingress:
    ingress.metadata.annotations["nginx.ingress.kubernetes.io/server-snippet"] = "if ($reject) { return 403; }"
+   ingress.metadata.annotations["nginx.ingress.kubernetes.io/auth-tls-secret"] = bundle_namespace + "/" + bundle_name
+   ingress.metadata.annotations["nginx.ingress.kubernetes.io/auth-tls-verify-client"] = "on"
    api_instance.patch_namespaced_ingress(name=ingress_name,namespace=ingress_namespace, body=ingress)
-  
-
-
-
+else: 
+   print("Ingress Rule is not existing in the namespace").
