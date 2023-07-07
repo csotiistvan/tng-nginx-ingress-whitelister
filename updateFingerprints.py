a3e623ee261349908c9717660c2c082794f9a3a4
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-07-07 15:37:03 +0200

    Log added

--- updateFingerprints.py
+++ updateFingerprints.py
@@ -10,20 +10,20 @@ with open(sys.argv[1]) as f:
 config_namespace = os.environ.get("CONFIG_NAMESPACE")
 config_name=os.environ.get("CONFIG_MAP")
 
-print(config_namespace)
-print(config_name)
-
-api_instance = client.CoreV1Api()
-cmap = api_instance.read_namespaced_config_map(name=config_name, namespace=config_namespace)
-seperator = "# WHITELIST"
-newBlock = "\n"+ seperator + data + "\n" + seperator
-if "http-snippet" in cmap.data:
-    existingconfig = cmap.data["http-snippet"]
-    map = existingconfig.split("seperator")
-    cmap.data["http-snippet"] = map[0]+ newBlock + map[2]
-else:
-    cmap.data["http-snippet"] = newBlock
-
-print(cmap)
-
-api_instance.patch_namespaced_config_map(namespace=config_namespace,name=config_name, body=cmap)
+if not config_namespace or not config_name:
+    print("Config Namespace or Config Map not configured")
+else: 
+    api_instance = client.CoreV1Api()
+    cmap = api_instance.read_namespaced_config_map(name=config_name, namespace=config_namespace)
+    seperator = "# WHITELIST"
+    newBlock = "\n"+ seperator + data + "\n" + seperator
+    if "http-snippet" in cmap.data:
+        existingconfig = cmap.data["http-snippet"]
+        map = existingconfig.split("seperator")
+        cmap.data["http-snippet"] = map[0]+ newBlock + map[2]
+    else:
+        cmap.data["http-snippet"] = newBlock
+    
+    print(cmap)
+    
+    api_instance.patch_namespaced_config_map(namespace=config_namespace,name=config_name, body=cmap)
