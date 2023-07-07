40802c732b8166e975c846eab06688f74ffb8d3c
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-07-07 07:26:50 +0200

    Update updateFingerprints.py

--- updateFingerprints.py
+++ updateFingerprints.py
@@ -7,9 +7,18 @@ config.load_incluster_config()
 with open(sys.argv[1]) as f:
     data = f.read()
 
-api_instance = client.CoreV1Api()
-cmap = client.V1ConfigMap()
-cmap.data = {}
-cmap.data["http-snippet"] = data
+config_namespace = os.environ.get("CONFIG_NAMESPACE")
+config_name=os.environ.get("CONFIG_MAP")
 
-api_instance.patch_namespaced_config_map(namespace=os.environ.get("CONFIG_NAMESPACE"),name=os.environ.get("CONFIG_MAP"), body=cmap)
+api_instance = client.CoreV1Api()
+cmap = api_instance.read_namespaced_config_map(namespace=config_namespace,name=config_name)
+seperator = "# WHITELIST"
+newBlock = "\n"+ seperator + data + "\n" + seperator
+if "http-snippet" in cmap.data:
+    existingconfig = cmap.data["http-snippet"]
+    map = existingconfig.split("seperator")
+    cmap.data["http-snippet"] = map[0]+ newBlock + map[2]
+else
+    cmap.data["http-snippet"] = newBlock
+
+api_instance.patch_namespaced_config_map(namespace=config_namespace,name=config_name, body=cmap)
