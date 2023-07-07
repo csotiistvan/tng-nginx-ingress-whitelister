8960300452bf7919a6a4a987a0452a59d283fb3b
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-07-07 17:14:49 +0200

    Fix in script

--- updateCaBundle.py
+++ updateCaBundle.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import base64
 from kubernetes import client, config
 
 config.load_incluster_config()
@@ -15,13 +16,14 @@ for file in files:
   with open(file) as f:
     data = f.read()
   ca_bundle = ca_bundle+"\n"+data
-print(ca_bundle)
   
+print(ca_bundle)
+ 
 if len(files) and ca_bundle:
   api_instance = client.CoreV1Api()
   body = client.V1Secret()
   body.api_version = 'v1'
-  body.data = ca_bundle
+  body.data = {'ca.crt': base64.b64encode(ca_bundle)}
   body.kind = 'Secret'
   body.type = 'Opaque'
   api_instance.patch_namespaced_secret(namespace=bundle_namespace,name=bundle_name, body=body)
