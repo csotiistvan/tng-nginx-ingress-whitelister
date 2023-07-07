2043f3e848621e5889f119ba244b29f3d33af07b
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-07-07 16:28:12 +0200

    Fingerprint Script corrected

--- fingerprints.sh
+++ fingerprints.sh
@@ -3,12 +3,12 @@
 nginx_conf="$1/nginx_conf"
 
 echo 'map $ssl_client_fingerprint $reject {' >> $nginx_conf
-echo "default 0;" >> $nginx_conf
+echo "default 1;" >> $nginx_conf
 
 for cert in $(/usr/bin/find $1 -path **/TLS/* -name TLS*.pem)
 do
     fingerprint=$(openssl x509 -in "$cert" -noout -fingerprint -sha1 | sed 's/SHA1 Fingerprint=//;s/sha1 Fingerprint=//; s/://g')
-    echo "$fingerprint 1;" >> $nginx_conf
+    echo "$fingerprint 0;" >> $nginx_conf
 done
 
 echo "}" >> $nginx_conf
