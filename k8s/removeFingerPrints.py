eb38db36e7f62eb4de5eca261e2e1972d74b3f1f
SchulzeStTSI <67590336+SchulzeStTSI@users.noreply.github.com>
2023-07-10 09:55:26 +0200

    removeFingerPrints added

---
+++ k8s/removeFingerPrints.py
@@ -0,0 +1,23 @@
+from kubernetes import client, config
+import sys
+import os
+
+config.load_incluster_config()
+
+config_namespace = os.environ.get("CONFIG_NAMESPACE")
+config_name=os.environ.get("CONFIG_MAP")
+config_seperator=os.environ.get("SEPERATOR")
+
+if not config_namespace or not config_name:
+    print("Config Namespace or Config Map not configured")
+else: 
+    api_instance = client.CoreV1Api()
+    cmap = api_instance.read_namespaced_config_map(name=config_name, namespace=config_namespace)
+    seperator = config_seperator
+    if "http-snippet" in cmap.data:
+        existingconfig = cmap.data["http-snippet"]
+        map = existingconfig.split(seperator)
+        if len(map) == 3:
+          cmap.data["http-snippet"] = map[0].strip() + map[2].strip()
+     
+    api_instance.patch_namespaced_config_map(namespace=config_namespace,name=config_name, body=cmap)
